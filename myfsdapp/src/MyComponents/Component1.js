33c0034e4314981bfa459b1c2c6d5a99c707d42d
abhishek <abhigautamls29@gmail.com>
2023-02-05 08:28:42 +0530

    Setting Up The Project

---
+++ myfsdapp/src/MyComponents/Component1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component1 = () => {
+    return (
+        <div>
+            <h1>Hello I'm Abhishek Gautam</h1>
+        </div>
+    );
+};
+
+export default Component1;
